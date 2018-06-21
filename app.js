4b6935433dc3224f0fb30b2579ba04323add2686
anameneses <anushkiss@hotmail.com>
2018-06-21 10:57:25 -0500

    creando colaboradores

---
+++ app.js
@@ -0,0 +1,13 @@
+let name = "meneses";
+let nombre = document.getElementById("nombre");
+let resultado;
+nombre.innerHTML = name
+for (i=0; i<name.length; i++){
+  let mostrar = name.charCodeAt(i);
+  console.log(mostrar);
+}
+
+
+/*let edad;
+edad = "Escribe tu edad";
+nombre.innerHTML = edad+" años de edad";*/
